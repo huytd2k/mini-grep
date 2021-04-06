64c1bb6fb49e34a9c45cee3071ec70c15f04b04e
Huy tran <huy.td183768@sis.hust.edu.vn>
2021-04-06 22:51:39 +0700

    update: finish

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::env;
+use std::process;
+
+use minigrep::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for: \"{}\"", config.query);
+    println!("In file {}", config.filename);
+
+    minigrep::run(config).unwrap_or_else(|err| {
+        eprintln!("Application error: {}",err);
+        process::exit(1);
+    })
 }
